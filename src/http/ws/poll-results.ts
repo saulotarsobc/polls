874c6addd40b3a46a92d2b9b9ed3a4dfab5bf97d
saulotarsobc <saulotarsobc@gmail.com>
2024-02-08 13:26:54 -0300

    add websocket class

--- src/http/ws/poll-results.ts
+++ src/http/ws/poll-results.ts
@@ -1,5 +1,9 @@
 import { FastifyInstance } from "fastify";
 
-export async function getPoll(app: FastifyInstance) {
-  app.get("/polls/:pollId/results", { websocket: true });
+export async function pollResults(app: FastifyInstance) {
+  app.get("/polls/:pollId/results", { websocket: true }, (connection, req) => {
+    connection.socket.on("message", (message: string) => {
+      // connection.socket.send("you sent: " + message);
+    });
+  });
 }
